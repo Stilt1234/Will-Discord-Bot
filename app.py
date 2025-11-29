2ee86ddcc4428997d1ab513b015efc84eface9d1
Stilt1234 <abhilashghosh2011@gmail.com>
2025-11-29 15:56:38 +0530

    Modified app.py

--- app.py
+++ app.py
@@ -1,7 +1,12 @@
 import gradio as gr
+import os
+from main import bot
 
 def greet(name):
     return "Hello " + name + "!!"
 
 demo = gr.Interface(fn=greet, inputs="text", outputs="text")
+
+bot.run(os.getenv("TOKEN"))
 demo.launch()
+print("hi")
