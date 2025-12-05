8fed5d0e3bbbbae3b1569a3f1bf6b12b322d5b59
Stilt1234 <abhilashghosh2011@gmail.com>
2025-12-05 21:05:19 +0530

    Added Will Cube Emoji and Discloud stuff

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
 import discord
 from discord.ext import commands
 import mcstatus
-from mcrcon import MCRcon
+from mcrcon import MCRcon, MCRconException
 from dotenv import load_dotenv, set_key
 import os
 from typing import Optional
+import socket
 
 load_dotenv(override=True)
 
@@ -12,6 +13,7 @@ bot = commands.Bot(command_prefix="/", description="Its Will! Use word WILL for 
 mc_server_ip = os.getenv("MC_SERVER_IP")
 mc_server_rcon_password = os.getenv("MC_SERVER_RCON_PASSWORD")
 will_emoji = None
+will_cube_emoji = None
 
 # TODO: Add /server_status, /playercount, /feed and /rcon_password (/rcon password will have both get and set options just like /server_ip)
 # TODO: Add error handling for admin only commands
@@ -28,10 +30,26 @@ def save_env_var(env_filepath=".env", key: str=None, value: str=None):
     set_key(env_filepath, key, value)
 
 # TODO: Add this to /say, /command, /rcon_password, /server_status and /playercount
-#def check_server_details(ctx: commands.Context, ip: str, rcon_password: str):
-    #try:
-        #with MCRcon(ip, rcon_password) as rcon:
-            #ctx.send(embed)
+
+# TODO: Use this in /rcon_password, /say and /announce
+def valid_server_details(ctx: commands.Context, ip: str, rcon_password: str):
+    if(ip == None or len(ip) == 0):
+        ctx.send("");
+    elif(rcon_password == None or len(rcon_password) == 0):
+    
+    try:
+        with MCRcon(ip, rcon_password) as rcon:
+            return True
+    except socket.gaierror:
+        ctx.send(embed=discord.Embed(colour=discord.Colour.red(), title="Server IP is incorrect.", timestamp=ctx.message.created_at))
+    except ConnectionRefusedError:
+        ctx.send(embed=discord.Embed(colour=discord.Colour.red(), title="Server is offline.", timestamp=ctx.message.created_at))
+    except MCRconException as e:
+        ctx.send(embed=discord.Embed(colour=discord.Colour.red(), title="RCON Password is incorrect.", timestamp=ctx.message.created_at))
+    except Exception as e:
+        ctx.send(embed=discord.Embed(colour=discord.Colour.red(), title=f"Unexpected error has occured : {e}.", timestamp=ctx.message.created_at))
+    
+    return False
 
 @bot.event
 async def on_ready():
@@ -46,8 +64,10 @@ async def on_message(message: discord.Message):
     # Check for dm.
     if(message.guild != None):
         will_emoji = discord.utils.get(message.guild.emojis, name = "will")
+        will_cube_emoji = discord.utils.get(message.guild.emojis, name="willcube")
         if(message.content.find("WILL") != -1 and will_emoji != None):
             await message.add_reaction(will_emoji)
+            await message.add_reaction(will_cube_emoji)
         elif(discord.utils.get(message.guild.emojis, name = "will") == None):
             await message.channel.send(embed=discord.Embed(colour=discord.Colour.red(), title="Please upload will emoji.", timestamp=message.created_at))
     await bot.process_commands(message)
@@ -70,6 +90,8 @@ async def recon_password(ctx: commands.Context, rcon_password: Optional[str]):
     if(not rcon_password):
         await ctx.send(embed=discord.Embed(colour=discord.Colour.green(), title=f"RCON Password is {mc_server_rcon_password}.", timestamp=ctx.message.created_at))
         return
+
+    valid_server_details()
 
     mc_server_rcon_password = rcon_password
     save_env_var(key="MC_SERVER_RCON_PASSWORD", value=mc_server_rcon_password)
